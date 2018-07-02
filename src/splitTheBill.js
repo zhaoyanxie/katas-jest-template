d3cbb75e3095f4a7451283d9518d13228c06ef61
zhaoyanxie <u076745e@alumni.nus.edu.sg>
2018-07-02 17:58:08 +0800

    Solution updated

--- src/splitTheBill.js
+++ src/splitTheBill.js
@@ -1,5 +1,19 @@
-const splitTheBill = () => {
-  return "something";
+const splitTheBill = people => {
+  // const keys = Object.keys(people);
+  const values = Object.values(people);
+  const average = values.reduce((a, b) => a + b) / values.length;
+
+  let billSplit = {};
+
+  for (let i in people) {
+    let result = people[i] - average;
+
+    if (result % 1 !== 0) {
+      result = Number.parseFloat(result).toFixed(2);
+    }
+    billSplit[i] = result;
+  }
+  return billSplit;
 };
 
 module.exports = splitTheBill;
--- test/splitTheBill.test.js
+++ test/splitTheBill.test.js
@@ -1,5 +1,13 @@
 const splitTheBill = require("../src/splitTheBill");
 
-it("should be replaced with a descriptive message", () => {
-  expect(splitTheBill()).toEqual("something");
+it("should equally split the bill", () => {
+  expect(splitTheBill({ A: 20, B: 15, C: 10 })).toEqual({ A: 5, B: 0, C: -5 });
+});
+
+it("should equally split the bill - 2", () => {
+  expect(splitTheBill({ A: 40, B: 25, X: 10 })).toEqual({
+    A: 15,
+    B: 0,
+    X: -15
+  });
 });
